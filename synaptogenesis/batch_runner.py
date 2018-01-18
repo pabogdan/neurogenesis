2fec0f11115cfb30842f3beb8fa0e3964229e687
Petrut Antoniu Bogdan <petrut.bogdan@student.manchester.ac.uk>
2018-01-18 16:38:32 +0000

    run lateral inhibition tests

--- synaptogenesis/batch_runner.py
+++ synaptogenesis/batch_runner.py
@@ -13,53 +13,54 @@ suffix = hashlib.md5(string_time).hexdigest()
 count = 0
 
 input_filename = "../simulation_statistics/2009_09_04.17_48_33 32Syn300s/InitialConnectivity.mat"
-iterations = 600000
+iterations = 1200000
 # t_record = iterations
 t_record = 30000
 
-cases = [3]
+cases = [1, 2, 3]
 # input_types = [1, 3, 4]
-lesion_types = [0, 1, 2]
+# lesion_types = [0, 1, 2]
 # sigma_stim = [.5,1., 1.5, 2., 2.5, 3.,3.5]
 no_runs = 1
+lateral_inhibition = 1
 
 parameters_of_interest = {
     'cases': cases,
     # 'input_types': input_types,
-    'lesion_types': lesion_types,
+    # 'lesion_types': lesion_types,
     'no_runs':no_runs,
+    'lateral_inhibition':lateral_inhibition,
     # 'sigma_stim':sigma_stim
 }
 
 log_calls = []
 
 for case in cases:
-    for lesion_type in lesion_types:
-        for run in range(no_runs):
-            filename = "case{}_lesiontype{}_run{}" \
-                       "_@{}".format(case,
-                                     lesion_type,
-                                     run + 1,
-                                     suffix)
-            count += 1
-            null = open(os.devnull, 'w')
-            print "Run ", count, "..."
+    for run in range(no_runs):
+        filename = "case{}_lateralin{}_run{}" \
+                   "_@{}".format(case,
+                                 lateral_inhibition,
+                                 run + 1,
+                                 suffix)
+        count += 1
+        null = open(os.devnull, 'w')
+        print "Run ", count, "..."
 
-            call = [sys.executable,
-                    'topographic_map_formation.py',
-                    '--case', str(case),
-                    '-i', input_filename,
-                    '-o', filename,
-                    '--no_iterations',
-                    str(iterations),
-                    '--t_record',
-                    str(t_record),
-                    '--lesion', str(lesion_type)
-                    ]
-            log_calls.append(call)
-            subprocess.call(call,
-                            stdout=null, stderr=null)
-            print "Run", count, "complete."
+        call = [sys.executable,
+                'topographic_map_formation.py',
+                '--case', str(case),
+                '-i', input_filename,
+                '-o', filename,
+                '--no_iterations',
+                str(iterations),
+                '--t_record',
+                str(t_record),
+                '--lat_inh', str(lateral_inhibition)
+                ]
+        log_calls.append(call)
+        subprocess.call(call,
+                        stdout=null, stderr=null)
+        print "Run", count, "complete."
 print "All done!"
 
 end_time = plt.datetime.datetime.now()
