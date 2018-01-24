e55757399b805cc2348e7cff248939fd4d4a6a20
Petrut Antoniu Bogdan <petrut.bogdan@postgrad.manchester.ac.uk>
2018-01-24 10:29:46 +0000

    sens an for sigma stim vs sigma form lat

--- synaptogenesis/batch_runner.py
+++ synaptogenesis/batch_runner.py
@@ -13,14 +13,15 @@ suffix = hashlib.md5(string_time).hexdigest()
 count = 0
 
 input_filename = "../simulation_statistics/2009_09_04.17_48_33 32Syn300s/InitialConnectivity.mat"
-iterations = 1200000
+iterations = 300000
 # t_record = iterations
-t_record = 30000
+t_record = 300000
 
-cases = [1, 2, 3]
+cases = [1]
 # input_types = [1, 3, 4]
-# lesion_types = [0, 1, 2]
-# sigma_stim = [.5,1., 1.5, 2., 2.5, 3.,3.5]
+# lesion_types = [1, 2]
+sigma_stims = [.5,1., 1.5, 2., 2.5, 3.,3.5]
+sigma_form_lats = [.5,1., 1.5, 2., 2.5, 3.,3.5]
 no_runs = 1
 lateral_inhibition = 1
 
@@ -29,38 +30,43 @@ parameters_of_interest = {
     # 'input_types': input_types,
     # 'lesion_types': lesion_types,
     'no_runs':no_runs,
-    'lateral_inhibition':lateral_inhibition,
-    # 'sigma_stim':sigma_stim
+    # 'lateral_inhibitions':lateral_inhibition,
+    'sigma_stims':sigma_stims,
+    'sigma_form_lats':sigma_form_lats
 }
 
 log_calls = []
 
 for case in cases:
-    for run in range(no_runs):
-        filename = "case{}_lateralin{}_run{}" \
-                   "_@{}".format(case,
-                                 lateral_inhibition,
-                                 run + 1,
-                                 suffix)
-        count += 1
-        null = open(os.devnull, 'w')
-        print "Run ", count, "..."
+    for sigma_stim in sigma_stims:
+        for sigma_form_lat in sigma_form_lats:
+            for run in range(no_runs):
+                filename = "case{}_sstim{}_sformlat{}_run{}" \
+                           "_@{}".format(case,
+                                         sigma_stim,
+                                         sigma_form_lat,
+                                         run + 1,
+                                         suffix)
+                count += 1
+                null = open(os.devnull, 'w')
+                print "Run ", count, "..."
 
-        call = [sys.executable,
-                'topographic_map_formation.py',
-                '--case', str(case),
-                '-i', input_filename,
-                '-o', filename,
-                '--no_iterations',
-                str(iterations),
-                '--t_record',
-                str(t_record),
-                '--lat_inh', str(lateral_inhibition)
-                ]
-        log_calls.append(call)
-        subprocess.call(call,
-                        stdout=null, stderr=null)
-        print "Run", count, "complete."
+                call = [sys.executable,
+                        'topographic_map_formation.py',
+                        '--case', str(case),
+                        '-i', input_filename,
+                        '-o', filename,
+                        '--no_iterations',
+                        str(iterations),
+                        '--t_record',
+                        str(t_record),
+                        '--sigma_stim', str(sigma_stim),
+                        '--sigma_form_lat', str(sigma_form_lat)
+                        ]
+                log_calls.append(call)
+                subprocess.call(call,
+                                stdout=null, stderr=null)
+                print "Run", count, "complete."
 print "All done!"
 
 end_time = plt.datetime.datetime.now()
