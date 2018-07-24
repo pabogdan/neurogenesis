635fca05f5085f0f85e6997348eecb6ced6d7167
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2018-07-24 10:57:45 +0100

    style changes. Awaiting OR approval.

--- sundry/random_balanced_network.py
+++ sundry/random_balanced_network.py
@@ -1,10 +1,8 @@
+from __future__ import print_function
 import spynnaker8 as sim
 import numpy as np
 from pyNN.utility.plotting import Figure, Panel
-# import matplotlib.pyplot as plt
 import pylab as plt
-
-from spynnaker8.utilities.neo_convertor import convert_spikes
 
 start_time = plt.datetime.datetime.now()
 v_reset = -65
@@ -14,10 +12,8 @@ parallel_safe = True
 rng = sim.NumpyRNG(seed=rngseed, parallel_safe=parallel_safe)
 
 sim_time = 5000
-connection_probability = .1
-connection_weight = 0.03
 coupling_multiplier = 2.
-delay = sim.RandomDistribution('uniform', [1, 14], rng=rng)
+delay_distribution = sim.RandomDistribution('uniform', [1, 14], rng=rng)
 
 cell_params_exc = {
     'tau_m': 20.0, 'cm': 1.0, 'v_rest': -65.0, 'v_reset': -65.0,
@@ -36,10 +32,9 @@ sim.setup(timestep=1, min_delay=1, max_delay=15)
 poisson_spike_source = sim.Population(500, sim.SpikeSourcePoisson(
     rate=50, duration=sim_time), label='poisson_source')
 
-# spike_times = [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000]
-spike_times = [sim_time // 2]
+# Inject a spike into the network half-way through the run
 spike_source_array = sim.Population(125, sim.SpikeSourceArray,
-                                    {'spike_times': spike_times},
+                                    {'spike_times': [sim_time // 2]},
                                     label='spike_source')
 
 # Neuronal populations
@@ -47,63 +42,60 @@ pop_exc = sim.Population(500, sim.IF_curr_exp(**cell_params_exc),
                          label='excitatory_pop')
 pop_inh = sim.Population(125, sim.IF_curr_exp(**cell_params_inh),
                          label='inhibitory_pop')
-uniformDistr = sim.RandomDistribution('uniform', [v_reset, v_thresh], rng=rng)
-pop_exc.set(v=uniformDistr)
+
+# Initialise cell membrane potential to some sub-threshold level
+pop_exc.set(v=sim.RandomDistribution('uniform',
+                                     [v_reset, v_thresh],
+                                     rng=rng))
 
 # Spike input projections
 spike_source_projection = sim.Projection(
     spike_source_array, pop_exc,
-    sim.FixedProbabilityConnector(p_connect=connection_probability),
-    sim.StaticSynapse(weight=2 * connection_weight, delay=delay),
+    sim.FixedProbabilityConnector(p_connect=.1),
+    synapse_type=sim.StaticSynapse(weight=0.06, delay=delay_distribution),
     receptor_type='excitatory')
+
 # Poisson source projections
 poisson_projection_exc = sim.Projection(
     poisson_spike_source, pop_exc,
-    sim.FixedProbabilityConnector(p_connect=connection_probability),
-    synapse_type=sim.StaticSynapse(weight=2 * connection_weight, delay=delay),
+    sim.FixedProbabilityConnector(p_connect=.1),
+    synapse_type=sim.StaticSynapse(weight=0.06, delay=delay_distribution),
     receptor_type='excitatory')
 poisson_projection_inh = sim.Projection(
     poisson_spike_source, pop_inh,
-    sim.FixedProbabilityConnector(p_connect=connection_probability),
-    sim.StaticSynapse(weight=connection_weight, delay=delay),
+    sim.FixedProbabilityConnector(p_connect=.1),
+    synapse_type=sim.StaticSynapse(weight=0.03, delay=delay_distribution),
     receptor_type='excitatory')
 
 # Recurrent projections
 exc_exc_rec = sim.Projection(
     pop_exc, pop_exc,
-    # sim.OneToOneConnector(),
-    sim.FixedProbabilityConnector(
-        p_connect=connection_probability),
-    synapse_type=sim.StaticSynapse(
-        weight=connection_weight, delay=delay),
+    sim.FixedProbabilityConnector(p_connect=.1),
+    synapse_type=sim.StaticSynapse(weight=0.03, delay=delay_distribution),
     receptor_type='excitatory')
 exc_exc_one_to_one_rec = sim.Projection(
     pop_exc, pop_exc,
     sim.OneToOneConnector(),
-    synapse_type=sim.StaticSynapse(
-        weight=connection_weight,
-        delay=delay),
+    synapse_type=sim.StaticSynapse(weight=0.03, delay=delay_distribution),
     receptor_type='excitatory')
 inh_inh_rec = sim.Projection(
     pop_inh, pop_inh,
-    sim.FixedProbabilityConnector(
-        p_connect=connection_probability),
-    synapse_type=sim.StaticSynapse(
-        weight=connection_weight, delay=delay),
+    sim.FixedProbabilityConnector(p_connect=.1),
+    synapse_type=sim.StaticSynapse(weight=0.03, delay=delay_distribution),
     receptor_type='inhibitory')
 
-# Neuronal population projections -- Coupling
+# Projections between neuronal populations
 exc_to_inh = sim.Projection(
     pop_exc, pop_inh,
-    sim.FixedProbabilityConnector(p_connect=2 * connection_probability),
+    sim.FixedProbabilityConnector(p_connect=.2),
     synapse_type=sim.StaticSynapse(
-        weight=coupling_multiplier * connection_weight, delay=delay),
+        weight=coupling_multiplier * 0.03, delay=delay_distribution),
     receptor_type='excitatory')
 inh_to_exc = sim.Projection(
     pop_inh, pop_exc,
-    sim.FixedProbabilityConnector(p_connect=2 * connection_probability),
+    sim.FixedProbabilityConnector(p_connect=.2),
     synapse_type=sim.StaticSynapse(
-        weight=coupling_multiplier * connection_weight, delay=delay),
+        weight=coupling_multiplier * 0.03, delay=delay_distribution),
     receptor_type='inhibitory')
 
 # Specify output recording
@@ -117,6 +109,10 @@ sim.run(simtime=sim_time)
 exc_data = pop_exc.get_data('spikes')
 inh_data = pop_inh.get_data('spikes')
 
+# Record duration of experiment
+end_time = plt.datetime.datetime.now()
+total_time = end_time - start_time
+
 # Plot spikes
 Figure(
     Panel(inh_data.segments[0].spiketrains,
@@ -127,19 +123,14 @@ Figure(
           marker="*", c='C0', xlabel="Time (ms)",
           xticks=np.linspace(0, sim_time, 5)),
     settings={'font.size': 14,
-              'savefig.dpi':600,
+              'savefig.dpi': 800,
               },
     title="Random Balanced Network"
 ).save("rbn.png")
 plt.show()
 
-# Exit simulation
+# Exit simulation -- release and clear the machine
 sim.end()
 
-end_time = plt.datetime.datetime.now()
-total_time = end_time - start_time
-
-exc_spikes = convert_spikes(exc_data)
-inh_spikes = convert_spikes(inh_data)
-
+# Report duration of experiment
 print("Total time elapsed -- " + str(total_time))
