7a16a156b33cf850118af2abe6d526ee6929fe05
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2018-12-22 19:51:35 +0200

    issue with angles

--- synaptogenesis/batch_runner.py
+++ synaptogenesis/batch_runner.py
@@ -72,7 +72,7 @@ for phase in PHASES:
                     '--no_iterations', str(iterations),
                     '--sigma_form_ff', str(sigma_form_ff),
                     '--sigma_form_lat', str(sigma_form_lat),
-                    '--training_angles 0 90 180 270'
+                    '--training_angles', str([0, 90, 180, 270])
                     ]
             log_calls.append(call)
             if concurrently_active_processes % MAX_CONCURRENT_PROCESSES == 0 \
