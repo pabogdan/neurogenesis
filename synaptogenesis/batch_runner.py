3de329c27ec4612583358d35dc9b339756f11e07
Petrut Antoniu Bogdan <petrut.bogdan@manchester.ac.uk>
2019-03-17 09:26:00 +0000

    different call list?

--- synaptogenesis/batch_runner.py
+++ synaptogenesis/batch_runner.py
@@ -73,7 +73,7 @@ for phase in PHASES:
                     '--no_iterations', str(iterations),
                     '--sigma_form_ff', str(sigma_form_ff),
                     '--sigma_form_lat', str(sigma_form_lat),
-                    '--training_angles', training_angles
+                    '--training_angles {}'.format(training_angles)
                     ]
             log_calls.append(call)
             if concurrently_active_processes % MAX_CONCURRENT_PROCESSES == 0 \
